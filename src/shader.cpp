704d649b6122ebc705f5ed539e49db4ad7f69c23
Jack Thomas <jthomas2780@student.pps.net>
2025-03-07 15:02:49 -0800

    purged

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,141 +1,46 @@
 #include "shader.hpp"
-#include <string>
-#include <regex>
 
-Shader::Shader(const char* vertexPath, const char* fragmentPath)
+unsigned int shader::compileShaderCode(const char* code, GLenum type)
 {
-    //load vertex and fragment shader code from file
-    std::string vertexCode;
-    std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
-    vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    try 
-    {
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
-        std::stringstream vShaderStream, fShaderStream;
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();		
-        vShaderFile.close();
-        fShaderFile.close();
-        vertexCode   = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch(std::ifstream::failure e)
-    {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-    }
-
-    //interpret vertex shader structs
-    for (std::smatch chunk : glsl_handler::applyRegex(vertexCode,glsl_basics::struct_regex)) {
-        std::string struct_name = chunk[1];
-        std::string contents = chunk[2];
-
-        std::vector<glsl_basics::ShaderInput> inputs;
-        for (std::smatch part : glsl_handler::applyRegex(contents,glsl_basics::struct_part_regex)) {
-            glsl_basics::ShaderType type = glsl_handler::stringToShaderType(part[1].str());
-            std::optional<int> size = (!part[3].str().empty()) ? std::optional<int>{std::stoi(part[3].str())} : std::nullopt;
-            inputs.push_back(glsl_basics::ShaderInput(type,part[2].str(),size,1));
-        }
-        structs.push_back(glsl_basics::ShaderStruct(struct_name,inputs));
-    }
-    for (auto x : structs) {
-        std::cout << x.name << std::endl;
-        for (auto y : x.inputs) {
-            std::cout << y.name << std::endl;
-            std::cout << (int)y.type << std::endl;
-        }
-    }
-    exit(-1);
-    
-    //interpret vertex shader code
-    /*
-    std::regex lrp(glsl_basics::layout_regex);
-    std::sregex_iterator chunk_start = std::sregex_iterator(vertexCode.begin(), vertexCode.end(), lrp);
-    std::sregex_iterator chunk_end = std::sregex_iterator();
-
-    std::string modifiedCode = vertexCode;
-    std::ptrdiff_t offset = 0;
-
-    int location = 0;
-    for (std::sregex_iterator i = chunk_start; i != chunk_end; ++i) {
-        std::smatch match = *i;
-        glsl_basics::ShaderType type = glsl_handler::stringToShaderType(match[1].str());
-        std::string modified = "layout (location = " + std::to_string(location) + ") " ;
-        modifiedCode.insert(match.position() + offset, modified);
-        types.push_back(type);
-        location += type.offset;
-        offset += modified.length();
-    }
-    vertexCode = modifiedCode;
-    */
-
-    //compile shaders
-    
-    const char* vShaderCode = vertexCode.c_str();
-    const char* fShaderCode = fragmentCode.c_str();
-
-    unsigned int vertex, fragment;
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &code, NULL);
+    glCompileShader(shader);
 
     int success;
     char infoLog[512];
-   
-    // vertex Shader
-    vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vShaderCode, NULL);
-    glCompileShader(vertex);
-    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-    if(!success)
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
     {
-        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    };
-    // fragment Shader
-    fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fShaderCode, NULL);
-    glCompileShader(fragment);
-    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-    if(!success)
-    {
-        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    };
-  
-    ID = glCreateProgram();
-    glAttachShader(ID, vertex);
-    glAttachShader(ID, fragment);
-    glLinkProgram(ID);
-    glGetProgramiv(ID, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(ID, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+        return -1;
     }
 
-    // delete the shaders
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
+    return shader;
 }
 
-void Shader::use() 
-{ 
-    glUseProgram(ID);
-}
-void Shader::setBool(const std::string &name, bool value) const
-{         
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
-}
-void Shader::setInt(const std::string &name, int value) const
-{ 
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
-}
-void Shader::setFloat(const std::string &name, float value) const
-{ 
-    glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
-}
-void Shader::setVector2(const std::string &name, float x, float y) const
+unsigned int shader::createShaderProgram(const char* vertexCode, const char* fragmentCode)
 {
-    glUniform2f(glGetUniformLocation(ID,name.c_str()),x,y);
+    unsigned int vertexShader = compileShaderCode(vertexCode, GL_VERTEX_SHADER);
+    unsigned int fragmentShader = compileShaderCode(fragmentCode, GL_FRAGMENT_SHADER);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        return -1;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
 }
--- src/shader.hpp
+++ src/shader.hpp
@@ -1,45 +1,9 @@
-#ifndef SHADER_H
-#define SHADER_H
-
-#include <glad/glad.h> // include glad to get all the required OpenGL headers
-
-#include <array>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
+#include <glad/glad.h>
 #include <iostream>
-#include <numeric>
+#include <vector>
 
-#include "glsl_basics.hpp"
-#include "glsl_handler.hpp"
+namespace shader {
 
-class Shader
-{
-private:
-    unsigned int ID;
-    std::vector<glsl_basics::ShaderStruct> structs;
-    std::vector<glsl_basics::ShaderInput> inputs;
-public:
-
-    Shader(const char* vertexPath, const char* fragmentPath);
-    
-    void use();
-    // utility uniform functions
-    void setBool(const std::string &name, bool value) const;  
-    void setInt(const std::string &name, int value) const;   
-    void setFloat(const std::string &name, float value) const;
-    void setVector2(const std::string &name, float x, float y) const;
-
-    int getShaderInputWidth() {
-        return std::accumulate(inputs.begin(),inputs.end(),0,[](int acc, glsl_basics::ShaderInput input) {
-            return acc + input.size;
-        });
-    }
-
-    int getShaderSegmentWidth(int index) {
-        return inputs[index].size;
-    }
+    unsigned int compileShaderCode(const char* code, GLenum type);
+    unsigned int createShaderProgram(const char* vertexCode, const char* fragmentCode);
 };
-  
-#endif
