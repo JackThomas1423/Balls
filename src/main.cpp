07b16902dbccbe051a468aa4ea82f7421c29bab4
Jack <Jthomas2780@student.pps.net>
2025-03-10 12:27:34 -0700

    shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "shader.hpp"
-#include "renderer.hpp"
 
 #include <iostream>
 #include <vector>
@@ -40,8 +38,6 @@ int main()
         return -1;
     }
 
-    //Shader main_shader("shaders/vertex.vs","shaders/fragment.fs");
-
     std::vector<float> vertices = {
         -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  
          0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
@@ -52,13 +48,30 @@ int main()
         0, 1, 2
     };
 
-    Renderer rend;
-    rend.BindBufferData(vertices,indices);
+    unsigned int VBO, VAO, EBO;
 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    // uncomment this call to draw in wireframe polygons.
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    float n = 0.0f;
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -66,11 +79,6 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        rend.RSO.use();
-        rend.RSO.setVector2("translation",n,0.0f);
-        rend.Render();
-
-        n += 0.0005f;
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
