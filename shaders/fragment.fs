555a6de75d8708019a08504740d97619384a40af
Jack Thomas <jthomas2780@student.pps.net>
2025-04-14 15:16:57 -0700

    started work on cam angling (should use class for this)

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -6,5 +6,5 @@ in vec2 TexCord;
 uniform sampler2D ourTexture;
 
 void main() {
-    FragColor = texture(ourTexture, TexCord) * vec4(0.8, 0.1, 0.6, 1.0);
+    FragColor = texture(ourTexture, TexCord) * vec4(0.5, 0.5, 0.5, 1.0);
 }
